fbee4d8baff350335aefa1f3c53473183b9460f1
guodh <guodh@gdh.com>
2013-05-26 18:53:28 +0800

    guodh20130526 product.js up

--- public/website/js/product.js
+++ public/website/js/product.js
@@ -23,14 +23,20 @@
 			$('.content').animate({height: 'toggle'},function(){
 			  $('.content').css({"display":"none"});
 		  	});
-		  	$('.content_none').css({"display":"block"});
+			$('.content_none').animate({height: 'toggle'},function(){
+				$('.content_none').css({"display":"block"});
+		  	});
+		  	
 		});
 		
 		$('.content_none').click(function(){
 			$('.content').animate({height: 'toggle'},function(){
 			  $('.content').css({"display":"block"});
 		  	});
-		  	$('.content_none').css({"display":"none"});
+			$('.content_none').animate({height: 'toggle'},function(){
+				$('.content_none').css({"display":"none"});
+		  	});
+		  	
 		});
 	}
 	
