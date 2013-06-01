7b7269f8ca7667351103c8a1d02d665b44301e1b
lijin@lijin.com <lijin@lijin.com>
2013-06-01 16:00:17 +0800

    fix bug

--- application/views/website/marking.php
+++ application/views/website/marking.php
@@ -2,10 +2,12 @@
 
 
 
-
- <script src="<?php echo base_url('public/website/js/supersized.3.2.7.min.js') ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("plugins/supersized/theme/supersized.shutter.js")?>"></script>
-    <script type="text/javascript">
+<script
+	src="<?php echo base_url('public/website/js/supersized.3.2.7.min.js') ?>"></script>
+<script
+	type="text/javascript"
+	src="<?php echo base_url("plugins/supersized/theme/supersized.shutter_marking.js")?>"></script>
+<script type="text/javascript">
         jQuery(function($){
         	 //菜单状态切换
             initFurniture();
@@ -31,39 +33,71 @@
 			});
 	    });
     </script>
-    
-    <style type="text/css">
-    	ul#slide-list {float: left;right: 2%;padding: 0px;position: absolute;margin: 0px; left: 68%;line-height: 42px;overflow: hidden; position: absolute;}
-    	ul#slide-list li {float: left;list-style: none outside none;margin: 0px;width: 12px;height: 40px;}
-    	.pic_main {height: 98px;}
-    </style>
-    </head>
+
+<style type="text/css">
+ul#slide-list {
+	float: left;
+	right: 2%;
+	padding: 0px;
+	position: absolute;
+	margin: 0px;
+	left: 68%;
+	line-height: 42px;
+	overflow: hidden;
+	position: absolute;
+}
+
+ul#slide-list li {
+	float: left;
+	list-style: none outside none;
+	margin: 0px;
+	width: 12px;
+	height: 40px;
+}
+
+.pic_main {
+	height: 98px;
+}
+</style>
+</head>
 
 <body>
-<div id="container">
-   <?php $this->load->view("website/common/top"); ?> 
-    <div class="pic_main">
-        <div class="pic_scroll">
-			<div id="thumb-tray" class="load-item">
-				<div id="thumb-back"></div>
-				<div id="thumb-forward"></div>
-			</div>
-        </div>
-        
-    </div>
-    
-    <!--内容图片说明-->
-	<div id="slidecaption" class="pic_con"></div>
-    <div class="main_bot">
-        <ul>
-            <li><a href="<?php echo site_url("w/history")?>">History</a></li>
-            <li><a href="#" style="color: #EC934A">Marking</a></li>
-        </ul>
-        <ul id="slide-list"></ul>
-    </div>
-	
-      <?php $this->load->view("website/common/footer"); ?>
+<div id="container"><?php $this->load->view("website/common/top"); ?>
+<div class="pic_main">
+<div class="pic_scroll">
+<div id="thumb-tray" class="load-item">
+<div id="thumb-back"></div>
+<div id="thumb-forward"></div>
 </div>
+</div>
+
+</div>
+
+<!--内容图片说明-->
+<div id="slidecaption" class="pic_con"></div>
+<div class="main_bot">
+<ul>
+	<li><a href="<?php echo site_url("w/history")?>">History</a></li>
+	<li><a href="#" style="color: #EC934A">Marking</a></li>
+</ul>
+<ul id="slide-list" style="display: none"></ul>
+</div>
+
+													<?php $this->load->view("website/common/footer"); ?></div>
 </body>
 </html>
-
+<script>
+//alert($(window).width()); //浏览器当前窗口可视区域宽度 
+</script>
+<style>
+.news_opactity2 {
+	background: url("../../public/website/images/black_mid.png") repeat
+		scroll 0 0 transparent;
+	height: 98px;
+	left: 0;
+	position: absolute;
+	top: 0;
+	width: 156px;
+}
+-->
+</style>
